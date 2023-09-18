b3124015c708ac924c71ea5cc76ab8c077ce3005
mikeaskew4 <mikeaskew4@gmail.com>
2023-09-18 13:49:47 -0500

    adds tflite

--- app/src/main/java/com/michaelaskew/avocadotimer/activities/CameraActivity.java
+++ app/src/main/java/com/michaelaskew/avocadotimer/activities/CameraActivity.java
@@ -1,8 +1,12 @@
 package com.michaelaskew.avocadotimer.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
@@ -11,9 +15,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.michaelaskew.avocadotimer.R;
+import com.michaelaskew.avocadotimer.utilities.ImageClassifierHelper;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +28,12 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 
+import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.task.vision.classifier.Classifications;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class CameraActivity extends AppCompatActivity {
@@ -39,6 +50,8 @@ public class CameraActivity extends AppCompatActivity {
     private RelativeLayout sliderFeedback;
     private SeekBar softnessSlider;
 
+    private ImageClassifierHelper imageClassifierHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +65,56 @@ public class CameraActivity extends AppCompatActivity {
         sliderFeedback = findViewById(R.id.sliderFeedback);
         softnessSlider = findViewById(R.id.softnessSlider);
 
+
+        initClassifier();
         initCamera();
+    }
+
+    private void initClassifier() {
+        imageClassifierHelper = ImageClassifierHelper.create(
+                this,
+                new ImageClassifierHelper.ClassifierListener() {
+                    @Override
+                    public void onError(String error) {
+                        // Handle error here
+                        Toast.makeText(CameraActivity.this, "Error: " + error, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResults(List<Classifications> results, long inferenceTime) {
+                        // Handle the results here
+                        if (results.size() > 0) {
+
+                            Log.d("CameraActivity", "***********************************");
+                            Log.d("CameraActivity", "***********************************");
+                            Log.d("CameraActivity", "Results: " + results.toString());
+                            Log.d("CameraActivity", "***********************************");
+                            Log.d("CameraActivity", "***********************************");
+
+
+                            boolean hasAvocado = false;
+                            ArrayList<String> displayNames = new ArrayList<>();
+                            for (org.tensorflow.lite.task.vision.classifier.Classifications classification : results) {
+                                for (org.tensorflow.lite.support.label.Category category : classification.getCategories()) {
+                                    displayNames.add(category.getLabel());
+                                    if (category.getLabel() == "avocado") {
+                                        hasAvocado = true;
+                                    }
+                                }
+                            }
+                            String toastMessage = "Hmm, we see a " + String.join(", a ", displayNames) + "... but not an avocado";
+                            if (hasAvocado) {
+                                toastMessage = "Confirmed avocado sighting!";
+                            }
+                            Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                            toast.show();
+                        }
+                    }
+                }
+        );
+
+
     }
 
     private void initCamera() {
@@ -72,10 +134,9 @@ public class CameraActivity extends AppCompatActivity {
 
                 preview.setSurfaceProvider(cameraPreviewView.getSurfaceProvider());
 
+
                 Button captureButton = findViewById(R.id.captureButton);
                 captureButton.setOnClickListener(v -> captureImage());
-
-
 
                 Button acceptImageButton = findViewById(R.id.acceptImageButton);
                 acceptImageButton.setOnClickListener(v -> {
@@ -114,16 +175,42 @@ public class CameraActivity extends AppCompatActivity {
     private void captureImage() {
         ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(new File(getExternalFilesDir(null), "capturedImage.jpg")).build();
 
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+
         imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
             @Override
             public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
                 capturedImageUri = Uri.fromFile(new File(outputFileResults.getSavedUri().getPath()));
+                Bitmap bitmap = BitmapFactory.decodeFile(capturedImageUri.getPath());
+                capturedImageView.setImageDrawable(null);
                 capturedImageView.setImageURI(capturedImageUri);
-
+                capturedImageView.invalidate();
                 preCaptureFeedback.setVisibility(View.GONE);
                 postCaptureFeedback.setVisibility(View.VISIBLE);
-            }
 
+                // Resizing the bitmap
+                int modelInputSize = 224; // Adjust this based on your model's input size
+                bitmap = Bitmap.createScaledBitmap(bitmap, modelInputSize, modelInputSize, true);
+
+                // Normalizing the bitmap (if needed)
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                int[] intValues = new int[width * height];
+                bitmap.getPixels(intValues, 0, width, 0, 0, width, height);
+                float[] floatValues = new float[width * height * 3];
+
+                for (int i = 0; i < intValues.length; i++) {
+                    final int val = intValues[i];
+                    floatValues[i * 3] = ((val >> 16) & 0xFF) / 255.0f;
+                    floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255.0f;
+                    floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
+                }
+
+                if (bitmap != null) {
+                    imageClassifierHelper.classify(bitmap, 0);
+                }
+
+            }
 
             @Override
             public void onError(@NonNull ImageCaptureException exception) {
